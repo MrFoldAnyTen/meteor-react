a680603babc2ec97cf3c075b63a8daab615ea14f
Dylan <dylanf@btinternet.com>
2015-12-30 15:54:46 +0000

    read tasks from DB

--- simple-todos-react/simple-todos-react.jsx
+++ simple-todos-react/simple-todos-react.jsx
@@ -1,5 +1,7 @@
-if (Meteor.isClient) {
+// Define a collection to hold our tasks
+Tasks = new Mongo.Collection("tasks");
 
+ if (Meteor.isClient) {
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     React.render(<App />, document.getElementById("render-target"));
